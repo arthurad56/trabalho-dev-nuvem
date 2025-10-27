2f80c03699765750179e755d5dddcd638ef9bc2e
Mateus <mateusosterne123@gmail.com>
2025-10-27 17:29:04 -0300

    criado trabalho

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express"); //importa o mÃ³dulo express neste arquivo
+const app = express(); //iniciando o express
+
+//criando a rota inicial
+app.get("/", function(req,res){
+    res.send("<h1>Bem vindo ao meu site!</h1>");
+})
+
+//rota do cadastro de cidades
+app.get("/cidades", function(req,res){
+    res.send("<h1>Lista de cidades!</h1>");
+})
+
+//rota com parametro 
+app.get("/consulta/:parametro", function(req,res){
+    //req --> dados enviados pelo cliente
+    //res --> resposta enviada pelo servidor de volta ao cliente
+    res.send("retorno consulta:" + req.params.parametro);
+})
+
+
+//rota com parametro opcional
+app.get("/cadastro/{:nome}", function(req,res){
+    //req --> dados enviados pelo cliente
+    var nome = req.params.nome;
+    if (nome){
+        res.send("<h1>cidade " + nome + " criada!</h1>");
+    }else{
+        res.send("cidade criada!");
+    }
+    
+})
+
+
+app.listen(process.env.PORT ?? 3000,function(erro){  // cria a aplicaÃ§Ã£o na porta 4000
+    if (erro){
+        console.log("Erro ao Iniciar.");
+    }else{
+        console.log("Servidor Iniciado. http://localhost:3000");
+    }
+})
